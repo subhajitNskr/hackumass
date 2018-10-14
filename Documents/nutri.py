4fb947bae659c6a702a0907ec5072792fe2e77e7
Abhishek Mandal <abhishekmandal1907@gmail.com>
2018-10-14 02:54:25 -0400

    Update nutri.py

--- Documents/nutri.py
+++ Documents/nutri.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sat Oct 13 01:21:26 2018
-
 @author: subha
 """
 
@@ -49,22 +48,39 @@ class NutritionManager:
 
     ##################To Be done###############################
     def  get_food_score(self, food):
-        with open('truncated_dict.pkl', 'rb') as td:
+        with open('../truncated_dict.pkl', 'rb') as td:
             truncated_dict = pickle.load(td)
-        with open('set_of_keywords.pkl', 'rb') as sk:
-            set_of_dict = pickle.load(sk)
+        with open('../set_of_keywords.pkl', 'rb') as sk:
+            set_of_keywords = pickle.load(sk)
             
         list_of_vals = [*truncated_dict]
         search_string = ""
-        for i in meal_string.split(" "):
+        for i in food.split(" "):
             if i in set_of_keywords:
                 search_string += (i + " ")
         
         search_string = search_string.strip()
-        print(truncated_dict[process.extract(search_string, list_of_vals, limit = 1)])
-        return truncated_dict[process.extract(search_string, list_of_vals, limit = 1)[0][0]]
-            
-        return calorie_val, colesterol_val, sugar_val, alcohol_val
+        # print(process.extract(search_string, list_of_vals, limit = 1))
+        index = truncated_dict[process.extract(search_string, list_of_vals, limit = 1)[0][0]]
+        row_0 = data_xls['Food code']
+        for r in range(len(row_0)):
+            if (row_0.loc[r]) == index:
+                row_val = r
+
+
+        calorie_val = 0
+        sugar_val = 0
+        colesterol_val = 0
+        fat_val = 0
+        try:
+            calorie_val = np_data[row_val, 2]
+            sugar_val = np_data[row_val, 4]
+            colesterol_val = np_data[row_val, 8]
+            fat_val = np_data[row_val, 7]
+        except:
+            KeyError
+        print("Individual Scores : ", calorie_val, colesterol_val, sugar_val, fat_val)
+        return calorie_val, colesterol_val, sugar_val, fat_val
     ###########################################################
 
 
@@ -104,18 +120,40 @@ class NutritionManager:
             print(user_info.get_user_history())
     
     def get_feedback(self, name, meal_type = None):
+        now = datetime.datetime.now()
+        date = str(now.day) + "-" + str(now.month) + "-" + str(now.year)
+        count = len(self.users[name].meal_dict[date])
+        if count == 0 :
+            return Exception("No food data")
         if meal_type != None:
             meal_score = self.get_meal_score(name, meal_type)
-            return meal_score.evaluate_feedback()
+            return meal_score.get_feedback(count)
         else:         
             meal_score = self.get_day_meal_score(name)
-            return meal_score.evaluate_feedback()
+            return meal_score.get_feedback(count)
     
     def return_food_histroy(self, name):
         if name in self.users:
-            print("food history")
+            # print("food history")
             r = UserInfoEncoder().encode(self.users[name].meal_dict)
-            print(r)
+            r = json.loads(r)
+            feedback = self.get_feedback(name)
+            day_activities = {}
+            for key in r:
+                print(type(r))
+                for meal in r[key]:
+                    if meal["meal_type"] == 'breakfast':
+                        day_activities["breakfast"] = meal["food_list"]
+                    if meal["meal_type"] == 'lunch':
+                        day_activities["lunch"] = meal["food_list"]
+                    if meal["meal_type"] == 'dinner':
+                        day_activities["dinner"] = meal["food_list"]
+
+                    day_activities["summary"] = feedback
+                    day_activities["day"] = datetime.datetime.today().strftime("%A")
+
+            print(day_activities)
+            return json.dumps(day_activities)
         
 class UserInfoEncoder(JSONEncoder):
     def default(self, o) :
@@ -169,7 +207,7 @@ class UserInfo:
             self.meal_dict[date] = meal_list
             self.day_dict[date] = MealScore()
             
-        meal_score = self.calculate_meal_score(self.meal_dict[date])
+        meal_score  = self.calculate_meal_score(self.meal_dict[date])
         self.update_meal_score(meal_score)
 
     
@@ -179,17 +217,17 @@ class UserInfo:
         self.day_dict[date].update_score(meal_score)
         
 ################################# TO BE DONE ##################################
-    def calculate_meal_score(self, meal):
+    def calculate_meal_score(self, meals):
         nutritionMgr = NutritionManager.getInstance()
         
         meal_score = MealScore()
         #if nutritionMgr == None:
         #    return meal_score
-        for food in meal:
-            score = nutritionMgr.get_food_score(id)
-            print(score)
-            meal_score.update_score_individual(score[0], score[1], score[2], score[3] )
-
+        for meal in meals:
+            for food in meal.get_food_list():
+                score = nutritionMgr.get_food_score(food)
+                # print(score)
+                meal_score.update_score_individual(score[0], score[1], score[2], score[3] )
         return meal_score
     
 """
@@ -222,9 +260,12 @@ class Meal:
     
     #debug utility
     def get_meal_info(self):
-        print(self.meal_type)
+        # print(self.meal_type)
         for food in self.food_list :
             print(food)
+    
+    def get_food_list(self):
+        return self.food_list
 
       
 
@@ -233,35 +274,32 @@ class MealScore:
         self.calorie_val = 0
         self.colesterol_val = 0
         self.sugar_val = 0
-        self.alcohol_val = 0
+        self.fat_val = 0
         self.feedback = ""
         
-    def set_score(self, calorie_val, colesterol_val, sugar_val, alcohol_val):
+    def set_score(self, calorie_val, colesterol_val, sugar_val, fat_val):
         self.calorie_val = calorie_val
         self.colesterol_val = colesterol_val
         self.sugar_val = sugar_val
-        self.alcohol_val = alcohol_val
-        self.evaluate_feedback()
+        self.fat_val = fat_val
     
-    def update_score_individual(self, calorie_val, colesterol_val, sugar_val, alcohol_val):
+    def update_score_individual(self, calorie_val, colesterol_val, sugar_val, fat_val):
         self.calorie_val += calorie_val
         self.colesterol_val += colesterol_val
         self.sugar_val += sugar_val
-        self.alcohol_val += alcohol_val
-        print("debug")
-        print(self.calorie_val)
+        self.fat_val += fat_val
+        # # print("debug")
+        # print(self.calorie_val)
         
-        self.evaluate_feedback()
         
     def update_score(self, meal_score):
         self.calorie_val = meal_score.calorie_val
         self.colesterol_val = meal_score.colesterol_val
         self.sugar_val = meal_score.sugar_val
-        self.alcohol_val = meal_score.alcohol_val
+        self.fat_val = meal_score.fat_val
         self.feedback = meal_score.feedback
-        print("debug")
-        print(self.calorie_val)
-        self.evaluate_feedback()
+        # print("debug")
+        # print(self.calorie_val)
         
     def get_calorie_val(self):
         return self.calorie_val
@@ -269,14 +307,61 @@ class MealScore:
         return self.colesterol_val 
     def sugar_val(self):
         return self.sugar_val 
-    def alcohol_val(self):
-        return self.alcohol_val
-    def get_feedback(self):
+    def fat_val(self):
+        return self.fat_val
+    def get_feedback(self, count):
+        if count == 0:
+            count = 1
+        self.evaluate_feedback(count)
         return self.feedback
     
-    def evaluate_feedback(self):
-        return ""
+    def evaluate_feedback(self, count):
+        self.calorie_val /= count
+        self.colesterol_val /= count
+        self.sugar_val /= count
+        self.fat_val /= count
+        print(self.calorie_val, self.colesterol_val, self.sugar_val, self.fat_val, count)
 
+        calorie_score = 0 
+        if self.calorie_val < 50:
+            calorie_score = -1
+        
+        elif self.calorie_val > 500:
+            calorie_score = 1
+
+        
+        sugar_score = -1
+        if self.sugar_val > 30:
+            sugar_score = 1
+
+        colesterol_score = -1
+        if self.colesterol_val > 25:
+            colesterol_score = 1
+
+        fat_score = -1
+        if self.fat_val > 78 / 3 :
+            fat_score = 1
+
+        if calorie_score == 0 and sugar_score == 0 and colesterol_score == 0 and fat_score ==0 :
+            self.feedback = "Wow, you have a really good food habit"
+
+        elif colesterol_score > 0 :
+            self.feedback = "You should watch out your cholestorol. Try some avocados maybe"
+
+        elif sugar_score > 0:
+            print(self.sugar_val, sugar_score)
+            self.feedback = "Stay away from cookies"
+
+        elif calorie_score > 0:
+            self.feedback = "Please reduce your food intake"
+
+        elif fat_score > 0:
+            self.feedback = "You are doing good, but maybe try some brocolli"
+
+        elif calorie_score < 0:
+            self.feedback = "Please eat something"
+        else :
+            self.feedback = "Good job"
 
 
 class UserScore:
@@ -300,11 +385,11 @@ class UserScore:
 
 
 pre = os.path.dirname(os.path.realpath(__file__))
-fname = 'FNDDS_food_value.xlsx'
+fname = 'dataset.xlsx'
 path = os.path.join(pre, fname)
-print(path)
+# print(path)
 data_xls = pd.read_excel(path)
-print(data_xls.shape)
+# print(data_xls.shape)
 data_xls.dropna()
 column_to_drop = data_xls.columns[49:68]
 data_xls.drop(column_to_drop, axis=1, inplace = True)
@@ -319,26 +404,22 @@ column_to_drop = data_xls.columns[47:48]
 data_xls.drop(column_to_drop, axis =1, inplace = True)
 column_to_drop = data_xls.columns[10:13]
 data_xls.drop(column_to_drop, axis =1, inplace = True)
-print(data_xls.shape)
+# print(data_xls.shape)
 #print(modify_name("milk, human"))
 
 np_data = data_xls.values
 #print(np_data)
-print("modified_ data")
-for i in range(1, np_data.shape[0]): #np_data.shape[0]):
-    np_data[i, 1] = modify_name(np_data[i, 1])
+# print("modified_ data")
+# for i in range(1, np_data.shape[0]): #np_data.shape[0]):
+#     np_data[i, 1] = modify_name(np_data[i, 1])
 
-print(np_data)
+# print(np_data)
 
 nutriInst = NutritionManager(np_data)
-nutriInst.add_user("aaa")
-nutriInst.add_meal("aaa", 100, ["rice, daal"], "lunch")
-nutriInst.add_meal("aaa", 100, ["rice, daal2"], "dinner")
-nutriInst.get_food_history("aaa")
-nutriInst.add_meal("bbb", 100, ["ricewe, daal"], "lunch")
-nutriInst.add_meal("bbb", 100, ["rice, daeral2"], "dinner")
-nutriInst.get_food_history("aaa")
+#nutriInst.add_meal("aaa", 100, ["rice, pork"], "dinner")
+# nutriInst.add_meal("bbb", 100, ["rice, eggs"], "lunch")
+nutriInst.add_meal("bbb", 100, ["cereal"], "dinner")
 nutriInst.get_food_history("bbb")
-nutriInst.get_meal_score("aaa", "lunch")
-nutriInst.return_food_histroy("aaa")
-print(nutriInst.get_day_meal_score("aaa").calorie_val)
+nutriInst.return_food_histroy("bbb")
+# print(nutriInst.get_day_meal_score("aaa").calorie_val)
+# print (nutriInst.get_feedback("aaa"))
